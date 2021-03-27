300b2f88c9368a24bdecd888d855ceaf0f24739a
Cezary Banach <cezarybanach@Cezarys-MacBook-Pro.local>
2021-03-27 14:17:17 +0100

    fixed import

--- src/WebUI/ClientApp/src/app/home/home.component.ts
+++ src/WebUI/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
-import { HotelClient } from '../../../../web-api-client';
+import { HotelClient } from 'src/app/web-api-client';
 
 import { QuoteService } from './quote.service';
 
@@ -17,8 +17,8 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.isLoading = true;
-    //const response = this.hotelClient.getHotelsWithPagination(0, 1, 10);
-    //console.log(response);
+    const response = this.hotelClient.getHotelsWithPagination(0, 1, 10);
+    console.log(response);
     this.quoteService
       .getRandomQuote({ category: 'dev' })
       .pipe(
