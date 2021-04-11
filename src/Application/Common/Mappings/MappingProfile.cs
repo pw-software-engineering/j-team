ad24c42a9817da47190e080fd9d5c33de46af93b
oskar1410 <o.nowak@student.mini.pw.edu.pl>
2021-04-11 15:58:40 +0200

    added back one more important map for member

--- src/Application/Common/Mappings/MappingProfile.cs
+++ src/Application/Common/Mappings/MappingProfile.cs
@@ -23,7 +23,8 @@ namespace HotelReservationSystem.Application.Common.Mappings
                 .ForMember(dest => dest.OfferPreviewPictureData, opt => opt.MapFrom(src => src.OfferPreviewPicture))
                 .ForMember(dest => dest.PicturesData, opt => opt.MapFrom(src => src.Pictures));
             CreateMap<Room, RoomDto>()
-                .ForMember(dest => dest.OffersData, opt => opt.MapFrom(src => src.Offers));
+                .ForMember(dest => dest.OffersData, opt => opt.MapFrom(src => src.Offers))
+                .ForMember(dest => dest.OfferID, opt => opt.MapFrom(src => src.Offers));
             CreateMap<Offer, int>()
                 .ConstructUsing(src => src.OfferId);
         }
