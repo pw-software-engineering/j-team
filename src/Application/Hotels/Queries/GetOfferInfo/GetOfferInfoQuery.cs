2c6d7bbebe3f0e367037e7826b4c779d853c132b
Jasiek Janiszewski <jasiekj1@gmail.com>
2021-05-30 15:45:40 +0200

    query added + tests
    requries adding datetime

---
+++ src/Application/Hotels/Queries/GetOfferInfo/GetOfferInfoQuery.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Application.Hotels;
+using HotelReservationSystem.Application.Common.Exceptions;
+using HotelReservationSystem.Application.Common.Interfaces;
+using MediatR;
+
+namespace HotelReservationSystem.Application.Hotels.Queries.GetOfferInfo
+{
+    public class GetOfferInfoQuery :IRequest<DetailedOfferDto>
+    {
+       public int hotelId;
+       public int offerId;
+    }
+
+    public class GetOfferInfoQueryHandler : IRequestHandler<GetOfferInfoQuery, DetailedOfferDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetOfferInfoQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<DetailedOfferDto> Handle(GetOfferInfoQuery request, CancellationToken cancellationToken)
+        {
+     
+            var offer = _context.Offers.Where(x => x.HotelId == request.hotelId && x.OfferId == request.offerId).FirstOrDefault();
+            if (offer == null) throw new NotFoundException();
+            var detailedOfferDto = new DetailedOfferDto
+            {
+                Title = offer.Title,
+                Description = offer.Description,
+                MaxGuests = offer.MaxGuests,
+                CostPerAdult = offer.CostPerAdult,
+                CostPerChild = offer.CostPerChild,
+                OfferPicturesData = offer.Pictures,
+                IsActive= offer.IsActive,
+                IsDeleted=offer.IsDeleted
+
+            };
+            return detailedOfferDto;
+        }
+
+    }
+}
---
+++ tests/Application.IntegrationTests/Hotels/Query/GetOfferInfoQueryTests.cs
@@ -0,0 +1,56 @@
+﻿using HotelReservationSystem.Application.Offers.Commands.CreateOffer;
+using HotelReservationSystem.Application.Hotels.Queries.GetOfferInfo;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using HotelReservationSystem.Application.Hotels.Commands.CreateHotel;
+using FluentAssertions;
+using HotelReservationSystem.Application.Common.Exceptions;
+
+
+namespace HotelReservationSystem.Application.IntegrationTests.Hotels.Query
+{
+    using static Testing;
+    class GetOfferInfoQueryTests
+    {
+
+
+        [Test]
+        public async Task ShouldReturnNotFoundException()
+        {
+            var query = new GetOfferInfoQuery { hotelId = 1 };
+
+            FluentActions.Invoking(() =>
+                SendAsync(query)).Should().Throw<NotFoundException>();
+
+        }
+
+        [Test]
+        public async Task ShouldReturnDetailedOfferInfo()
+        {
+            var hotelId = await SendAsync(new CreateHotelCmd
+            {
+                Name = "hotel1",
+                City = "city",
+                Country = "country",
+                Password = "hotel1"
+            });
+            var offerId = await SendAsync(new CreateOfferCmd
+            {
+                OfferTitle="SuperOferta",
+                HotelId=hotelId,
+                Description="Wakacje pod palmami"
+
+            });
+
+            var result = await SendAsync(new GetOfferInfoQuery
+            {
+                hotelId = hotelId,
+                offerId =offerId
+            });
+            result.Should().NotBeNull();
+            result.Title.Should().Be("SuperOferta");
+            
+
+        }
+    }
+}
